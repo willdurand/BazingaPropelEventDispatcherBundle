01474fefa53b0401fdafc51f7bbb36d1dc2229d7
William DURAND <william.durand1@gmail.com>
2012-07-16 23:05:04 +0200

    Add tests to prove #3

--- Tests/BazingaPropelEventDispatcherBundleTest.php
+++ Tests/BazingaPropelEventDispatcherBundleTest.php
@@ -3,17 +3,16 @@
 namespace Bazinga\Bundle\PropelEventDispatcherBundle\Tests;
 
 use Bazinga\Bundle\PropelEventDispatcherBundle\Tests\Fixtures\Model\MyObject;
+use Bazinga\Bundle\PropelEventDispatcherBundle\Tests\Fixtures\Model\MyObject3;
 
 class BazingaPropelEventDispatcherBundleTest extends WebTestCase
 {
-    public function setUp()
+    public function testGetListener()
     {
         $this->assertFalse(class_exists('Bazinga\Bundle\PropelEventDispatcherBundle\Tests\Fixtures\Model\MyObject', false));
         $this->assertFalse(class_exists('Bazinga\Bundle\PropelEventDispatcherBundle\Tests\Fixtures\Model\MyObject2', false));
-    }
+        $this->assertFalse(class_exists('Bazinga\Bundle\PropelEventDispatcherBundle\Tests\Fixtures\Model\MyObject3', false));
 
-    public function testGetListener()
-    {
         $listener = $this->getContainer()->get('listener.my_event_listener');
 
         $this->assertNotNull($listener);
@@ -22,6 +21,10 @@ class BazingaPropelEventDispatcherBundleTest extends WebTestCase
 
     public function testGetListenerWithNonExistentClass()
     {
+        $this->assertFalse(class_exists('Bazinga\Bundle\PropelEventDispatcherBundle\Tests\Fixtures\Model\MyObject', false));
+        $this->assertFalse(class_exists('Bazinga\Bundle\PropelEventDispatcherBundle\Tests\Fixtures\Model\MyObject2', false));
+        $this->assertFalse(class_exists('Bazinga\Bundle\PropelEventDispatcherBundle\Tests\Fixtures\Model\MyObject3', false));
+
         $listener = $this->getContainer()->get('listener.my_event_listener_2');
 
         $this->assertNotNull($listener);
@@ -30,13 +33,39 @@ class BazingaPropelEventDispatcherBundleTest extends WebTestCase
 
     public function testGetListenerWithNonExistentParentClass()
     {
+        $this->assertFalse(class_exists('Bazinga\Bundle\PropelEventDispatcherBundle\Tests\Fixtures\Model\MyObject', false));
+        $this->assertFalse(class_exists('Bazinga\Bundle\PropelEventDispatcherBundle\Tests\Fixtures\Model\MyObject2', false));
+        $this->assertFalse(class_exists('Bazinga\Bundle\PropelEventDispatcherBundle\Tests\Fixtures\Model\MyObject3', false));
+
         $listener = $this->getContainer()->get('listener.my_event_listener_3');
     }
 
     public function testFireEvent()
     {
+        $this->assertFalse(class_exists('Bazinga\Bundle\PropelEventDispatcherBundle\Tests\Fixtures\Model\MyObject', false));
+        $this->assertFalse(class_exists('Bazinga\Bundle\PropelEventDispatcherBundle\Tests\Fixtures\Model\MyObject2', false));
+        $this->assertFalse(class_exists('Bazinga\Bundle\PropelEventDispatcherBundle\Tests\Fixtures\Model\MyObject3', false));
+
         $object   = new MyObject();
         $listener = $this->getContainer()->get('listener.my_event_listener');
+
+        $this->assertCount(0, $listener->getEvents());
+
+        $object->preSave();
+        $this->assertCount(1, $listener->getEvents());
+
+        $events  = $listener->getEvents();
+        $subject = $events[0]->getSubject();
+        $this->assertSame($object, $subject);
+    }
+
+    public function testFireEventWithEarlyBoot()
+    {
+        $this->assertFalse(class_exists('Bazinga\Bundle\PropelEventDispatcherBundle\Tests\Fixtures\Model\MyObject2', false));
+        $this->assertFalse(class_exists('Bazinga\Bundle\PropelEventDispatcherBundle\Tests\Fixtures\Model\MyObject3', false));
+
+        $listener = $this->getContainer()->get('listener.my_event_listener_4');
+        $object   = new MyObject3();
 
         $this->assertCount(0, $listener->getEvents());
 
---
+++ Tests/Fixtures/Model/MyObject3.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Bazinga\Bundle\PropelEventDispatcherBundle\Tests\Fixtures\Model;
+
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+use Symfony\Component\EventDispatcher\GenericEvent;
+
+class MyObject3 implements \EventDispatcherAwareModelInterface
+{
+    static private $eventDispatcher;
+
+    /**
+     * {@inheritdoc}
+     */
+    static public function setEventDispatcher(EventDispatcherInterface $eventDispatcher)
+    {
+        self::$eventDispatcher = $eventDispatcher;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    static public function getEventDispatcher()
+    {
+        return self::$eventDispatcher;
+    }
+
+    public function preSave()
+    {
+        self::$eventDispatcher->dispatch('propel.pre_save', new GenericEvent($this));
+    }
+}
